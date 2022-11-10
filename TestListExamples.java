9d0b769620b59425a61c063998ea7163a94f3d40
jic035 <jic035@ucsd.edu>
2022-11-09 22:06:56 -0800

    lab

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,13 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  List l = new ArrayList<>();
+  
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,14 @@
 # Create your grading script here
-
 set -e
+CPATH = ".:lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
-
+cp GradeServer.java student-submission/
+cd student-submission
+javac -cp $CPATH *.java
+if [[ -e ListExamples.java ]]
+then
+    echo "file exists"
+else
+    echo "not exist"
